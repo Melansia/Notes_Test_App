ebb6ef89f3afabd3e644b33211c9f6c747f7e0e0
Melansia <lupasco.grigori@gmail.com>
2022-08-23 14:02:57 +0300

    functionality to add a note

--- app/src/main/java/com/example/notesapp/AddNoteActivity.kt
+++ app/src/main/java/com/example/notesapp/AddNoteActivity.kt
@@ -1,14 +1,57 @@
 package com.example.notesapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+
 
 class AddNoteActivity : AppCompatActivity() {
+
+    private lateinit var edTitle: EditText
+    private lateinit var edDescription: EditText
+    private lateinit var spinnerDaysOfWeek: Spinner
+    private lateinit var rgPriority: RadioGroup
+    private lateinit var btnSaveNote: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_note)
+
+        edTitle = findViewById(R.id.edTitle)
+        edDescription = findViewById(R.id.edDescription)
+        spinnerDaysOfWeek = findViewById(R.id.spinnerDaysOfWeek)
+        rgPriority = findViewById(R.id.rgPriority)
+        btnSaveNote = findViewById(R.id.btnSaveNote)
+
+        btnSaveNote.setOnClickListener {
+            val title = edTitle.text.toString().trim()
+            val description = edDescription.text.toString().trim()
+            val dayOfWeek = spinnerDaysOfWeek.selectedItem.toString()
+            val radioButton = findViewById<RadioButton>(rgPriority.checkedRadioButtonId)
+            val priority = radioButton.text.toString().toInt()
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("EXTRA_TITLE", title)
+            intent.putExtra("EXTRA_DESCRIPTION", description)
+            intent.putExtra("EXTRA_WEEKDAY", dayOfWeek)
+            intent.putExtra("EXTRA_PRIORITY", priority)
+            startActivity(intent)
+        }
     }
 
-    fun onClickSaveNote(view: View) {}
+
+//    fun onClickSaveNote(view: View?) {
+//        val title = edTitle.text.toString().trim()
+//        val description = edDescription.text.toString().trim()
+//        val dayOfWeek = spinnerDaysOfWeek.selectedItem.toString()
+//        val radioButton = findViewById<RadioButton>(rgPriority.checkedRadioButtonId)
+//        val priority = radioButton.text.toString().toInt()
+//        val intent = Intent(this, MainActivity::class.java)
+//        intent.putExtra("EXTRA_TITLE", title)
+//        intent.putExtra("EXTRA_DESCRIPTION", description)
+//        intent.putExtra("EXTRA_WEEKDAY", dayOfWeek)
+//        intent.putExtra("EXTRA_PRIORITY", priority)
+//        startActivity(intent)
+//    }
 }
--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.example.notesapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+open class MainActivity : AppCompatActivity() {
 
     private lateinit var rvNotes: RecyclerView
-    val notes = ArrayList<Note>()
-    private lateinit var adapter : NotesAdapter
+    val notes: ArrayList<Note> = ArrayList()
+    private lateinit var adapter: NotesAdapter
+
+
+    private lateinit var title: String
+    private lateinit var description: String
+    private lateinit var dayOfWeek: String
+    private var priority: Int = 3
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,17 +25,29 @@ class MainActivity : AppCompatActivity() {
 
         rvNotes = findViewById(R.id.rvNotes)
 
-        notes.add(Note("Market","Buy stuff for lemonade","Sunday", 2))
-        notes.add(Note("Dentist","Call Vasilis to close a visit","Monday", 3))
-        notes.add(Note("Skroutz","Pick up Moms Tablet","Tuesday", 2))
-        notes.add(Note("Sea","Trip to Voliagmenis Lake","Wednesday", 3))
-        notes.add(Note("Market","Buy Coffee","Thursday", 2))
-        notes.add(Note("Monastiraki","Meet with Petir","Friday", 1))
-        notes.add(Note("Syntagma","Check for Games","Saturday", 1))
+        if (notes.isEmpty()) {
+            notes.add(Note("Market", "Buy stuff for lemonade", "Sunday", 2))
+            notes.add(Note("Dentist", "Call Vasilis to close a visit", "Monday", 3))
+            notes.add(Note("Skroutz", "Pick up Moms Tablet", "Tuesday", 2))
+            notes.add(Note("Sea", "Trip to Voliagmenis Lake", "Wednesday", 3))
+            notes.add(Note("Market", "Buy Coffee", "Thursday", 2))
+            notes.add(Note("Monastiraki", "Meet with Petir", "Friday", 1))
+            notes.add(Note("Syntagma", "Check for Games", "Saturday", 1))
+        }
 
         adapter = NotesAdapter(notes)
         rvNotes.layoutManager = LinearLayoutManager(this)
         rvNotes.adapter = adapter
+
+        if (intent != null && intent.hasExtra("EXTRA_PRIORITY")) {
+            title = intent.getStringExtra("EXTRA_TITLE").toString()
+            description = intent.getStringExtra("EXTRA_DESCRIPTION").toString()
+            dayOfWeek = intent.getStringExtra("EXTRA_WEEKDAY").toString()
+            priority = intent.getIntExtra("EXTRA_PRIORITY", 1)
+            val note = Note(title, description, dayOfWeek, priority)
+            notes.add(note)
+        }
+
     }
 
     fun onClickAddNote(view: View) {
