83ece23e5888ae60dc38a923a919be6d0ef7048e
Melansia <lupasco.grigori@gmail.com>
2022-08-24 18:32:22 +0300

    set up database

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.notesapp
 
 import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
@@ -15,6 +17,7 @@ open class MainActivity : AppCompatActivity() {
     private lateinit var rvNotes: RecyclerView
     val notes: ArrayList<Note> = ArrayList()
     private lateinit var adapter: NotesAdapter
+    private lateinit var dbHelper: NotesDBHelper
 
 
     private lateinit var title: String
@@ -22,34 +25,60 @@ open class MainActivity : AppCompatActivity() {
     private lateinit var dayOfWeek: String
     private var priority: Int = 3
 
+    @SuppressLint("Range")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        dbHelper = NotesDBHelper(this)
+        val database: SQLiteDatabase = dbHelper.writableDatabase
 
         rvNotes = findViewById(R.id.rvNotes)
 
-        if (notes.isEmpty()) {
-            notes.add(Note("Market", "Buy stuff for lemonade", "Sunday", 2))
-            notes.add(Note("Dentist", "Call Vasilis to close a visit", "Monday", 3))
-            notes.add(Note("Skroutz", "Pick up Moms Tablet", "Tuesday", 2))
-            notes.add(Note("Sea", "Trip to Voliagmenis Lake", "Wednesday", 3))
-            notes.add(Note("Market", "Buy Coffee", "Thursday", 2))
-            notes.add(Note("Monastiraki", "Meet with Petir", "Friday", 1))
-            notes.add(Note("Syntagma", "Check for Games", "Saturday", 1))
+//        if (notes.isEmpty()) {
+//            notes.add(Note("Market", "Buy stuff for lemonade", "Sunday", 2))
+//            notes.add(Note("Dentist", "Call Vasilis to close a visit", "Monday", 3))
+//            notes.add(Note("Skroutz", "Pick up Moms Tablet", "Tuesday", 2))
+//            notes.add(Note("Sea", "Trip to Voliagmenis Lake", "Wednesday", 3))
+//            notes.add(Note("Market", "Buy Coffee", "Thursday", 2))
+//            notes.add(Note("Monastiraki", "Meet with Petir", "Friday", 1))
+//            notes.add(Note("Syntagma", "Check for Games", "Saturday", 1))
+//        }
+
+//        if (intent != null && intent.hasExtra("EXTRA_PRIORITY")) {
+//            title = intent.getStringExtra("EXTRA_TITLE").toString()
+//            description = intent.getStringExtra("EXTRA_DESCRIPTION").toString()
+//            dayOfWeek = intent.getStringExtra("EXTRA_WEEKDAY").toString()
+//            priority = intent.getIntExtra("EXTRA_PRIORITY", 1)
+//            val note = Note(title, description, dayOfWeek, priority)
+//            notes.add(note)
+//        }
+
+        for (note in notes) {
+            val contentValues = ContentValues()
+            contentValues.put(NotesContract.NotesEntry.COLUMN_TITLE, note.title)
+            contentValues.put(NotesContract.NotesEntry.COLUMN_DESCRIPTION, note.description)
+            contentValues.put(NotesContract.NotesEntry.COLUMN_DAY_OF_WEEK, note.dayOfWeek)
+            contentValues.put(NotesContract.NotesEntry.COLUMN_PRIORITY, note.priority)
+            database.insert(NotesContract.NotesEntry.TABLE_NAME, null, contentValues)
         }
 
-        adapter = NotesAdapter(notes)
+        val notesFromDB = ArrayList<Note>()
+        val cursor =
+            database.query(NotesContract.NotesEntry.TABLE_NAME, null, null, null, null, null, null)
+        while (cursor.moveToNext()) {
+            val title = cursor.getString(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_TITLE))
+            val description = cursor.getString(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_DESCRIPTION))
+            val dayOfWeek = cursor.getString(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_DAY_OF_WEEK))
+            val priority = cursor.getInt(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_PRIORITY))
+            val note = Note(title, description,dayOfWeek,priority)
+            notesFromDB.add(note)
+        }
+        cursor.close()
+
+        adapter = NotesAdapter(notesFromDB)
         rvNotes.layoutManager = LinearLayoutManager(this)
         rvNotes.adapter = adapter
 
-        if (intent != null && intent.hasExtra("EXTRA_PRIORITY")) {
-            title = intent.getStringExtra("EXTRA_TITLE").toString()
-            description = intent.getStringExtra("EXTRA_DESCRIPTION").toString()
-            dayOfWeek = intent.getStringExtra("EXTRA_WEEKDAY").toString()
-            priority = intent.getIntExtra("EXTRA_PRIORITY", 1)
-            val note = Note(title, description, dayOfWeek, priority)
-            notes.add(note)
-        }
 
         adapter.setOnNoteClickListener(object : NotesAdapter.OnNoteClickListener {
             override fun onNoteClick(position: Int) {
---
+++ app/src/main/java/com/example/notesapp/NotesDBHelper.kt
@@ -0,0 +1,24 @@
+package com.example.notesapp
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class NotesDBHelper(
+    context: Context?
+) : SQLiteOpenHelper(context, DB_NAME, null, DB_VERSION) {
+
+    companion object {
+        private val DB_NAME = "notes.db"
+        private val DB_VERSION = 1
+    }
+
+    override fun onCreate(p0: SQLiteDatabase?) {
+        p0?.execSQL(NotesContract.NotesEntry.CREATE_COMMAND)
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        p0?.execSQL(NotesContract.NotesEntry.DROP_COMMAND)
+        onCreate(p0)
+    }
+}
